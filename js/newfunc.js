82b831ab3e4467351d6fefe5965292610a8e88ef
marcovr27 <marcovr27@hotmail.com>
2017-10-10 16:18:20 -0700

    separate javascript

---
+++ js/newfunc.js
@@ -0,0 +1,35 @@
+///////<<<<<<<<<<<<============================= LOGBOOK PAGE =========================================>>>>>>>>>>>///////
+function filltaskworked()
+{
+	var db = window.openDatabase("Fieldtracker", "1.0", "Fieldtracker", 50000000);
+    db.transaction(Querytaskworked, errorCB);
+	
+}
+
+function Querytaskworked(tx)
+{
+  tx.executeSql("SELECT * FROM DUTIES2TASKS ORDER BY Duty,OrdNum", [], QuerytaskworkedSuccess, errorCB);
+}
+
+function QuerytaskworkedSuccess(tx, results)
+{
+	var len = results.rows.length;
+	alert(len);
+	var selecthtml='<option value="0">Choose a task</option>';
+	if(len>0)
+	{
+		for (var i=0; i<len; i++){
+			 selecthtml+='<option value="'+results.rows.item(i).Duty+'">'+results.rows.item(i).TaskID+'</option>';
+             }
+		 $("#select_taskworked").html(selecthtml);	 
+	}
+	else
+	{
+	  navigator.notification.alert("No registered tasks", null, 'FieldTracker', 'Accept'); 
+	}
+	
+	
+}
+
+
+///////<<<<<<<<<<<<=============================END FUNCTION LOGBOOK PAGE  =========================================>>>>>>>>>>>///////
